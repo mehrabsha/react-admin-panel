58b5a0024a9248b5973c57a4cd053fa35fb05096
Alireza SharifPour <a.sharifpour@yourypto.com>
2021-08-24 10:51:47 +0430

    Initial commit with Default Metronic 8.0.23 files

---
+++ src/node_modules/@manaflair/redux-batch/build/core/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _reduxBatch = require('./reduxBatch');
+
+Object.defineProperty(exports, 'reduxBatch', {
+  enumerable: true,
+  get: function get() {
+    return _reduxBatch.reduxBatch;
+  }
+});
