c78fef8e11485eb0c931bdfbdd395a4f01617d7e
mehrab shayetseh <m.shayesteh@yourypto.com>
2021-08-29 11:39:05 +0430

    add formik

--- src/src/app/modules/withdraws/WithdrawsFilters.tsx
+++ src/src/app/modules/withdraws/WithdrawsFilters.tsx
@@ -1,6 +1,6 @@
 import Select from 'react-select';
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useState } from 'react';
+import { useFormik } from 'formik';
+import { useHistory } from 'react-router-dom';
 
 const statusOptions = [
   { value: 'pending', label: 'Pending' },
@@ -17,7 +17,33 @@ const currncyOptions = [
   { value: 'safemoon', label: 'safemoon' }
 ];
 
+interface optionItems {
+  value: string;
+  label: string;
+}
+
+const selectDefaultValue = (options: optionItems[], value: string) =>
+  options
+    ? options.find(option => option.value === value)
+    : { value: '', label: '' };
+
 const WithdrawsFilters = () => {
+  const history = useHistory();
+  const formik = useFormik({
+    initialValues: {
+      id: '',
+      currency: 'btc',
+      status: 'approved'
+    },
+    onSubmit: values => {
+      console.log(values);
+      history.push({
+        // search: `?id=${values.id}&currency=${values.crypto}`
+        search: `?${new URLSearchParams(values).toString()}`
+      });
+    }
+  });
+
   return (
     <div className="card card-xl-stretch mb-5 mb-xl-8">
       {/* begin::Header */}
@@ -29,37 +55,87 @@ const WithdrawsFilters = () => {
       <div className="card-body pt-0">
         <form>
           <div className="row">
-            <div className="col-4">
-              <div className="form-group">
-                <label htmlFor="userid">Id</label>
-                <input
-                  className="form-control"
-                  id="userid"
-                  name="id"
-                  placeholder="12345"
-                />
-              </div>
+            <div className="col-4 form-group my-2">
+              <label htmlFor="withdrawid">Withdraw id</label>
+              <input
+                className="form-control"
+                id="withdrawid"
+                name="id"
+                placeholder="12345"
+                onChange={formik.handleChange}
+                value={formik.values.id}
+              />
             </div>
-            <div className="col-4 ">
+            <div className="col-4  form-group my-2">
               <label>Currency</label>
               <Select
-                //value={selectedStatus}
-                onChange={e => {
-                  console.log(e);
-                }}
+                value={selectDefaultValue(
+                  currncyOptions,
+                  formik.values.currency
+                )}
+                onChange={value =>
+                  formik.setFieldValue('currency', value?.value)
+                }
+                name="currency"
                 options={currncyOptions}
                 placeholder="select currency ..."
               />
             </div>
-            <div className="col-4 form-group">
+            <div className="col-4 form-group my-2">
               <label>Status</label>
               <Select
-                //value={selectedStatus}
-                onChange={e => {
-                  console.log(e);
-                }}
+                value={selectDefaultValue(statusOptions, formik.values.status)}
+                onChange={value => formik.setFieldValue('status', value?.value)}
+                name="status"
                 options={statusOptions}
                 placeholder="select status ..."
+              />
+            </div>
+
+            <div className="col-6 form-group my-2">
+              <label htmlFor="user">User</label>
+              <input
+                className="form-control"
+                id="user"
+                name="user"
+                placeholder="first name , last name , mobile , ..."
+              />
+            </div>
+            <div className="col-3 form-group my-2">
+              <label htmlFor="network">From date</label>
+              <input
+                className="form-control"
+                id="network"
+                name="network"
+                placeholder="2021/10/08"
+              />
+            </div>
+            <div className="col-3 form-group my-2">
+              <label htmlFor="network">To date</label>
+              <input
+                className="form-control"
+                id="network"
+                name="network"
+                placeholder="2021/12/04"
+              />
+            </div>
+
+            <div className="col-8 form-group my-2">
+              <label htmlFor="network">Tx id</label>
+              <input
+                className="form-control"
+                id="network"
+                name="network"
+                placeholder="2021/12/04"
+              />
+            </div>
+            <div className="col-4  form-group my-2">
+              <label htmlFor="network">Network</label>
+              <input
+                className="form-control"
+                id="network"
+                name="network"
+                placeholder="bep20 ,..."
               />
             </div>
           </div>
