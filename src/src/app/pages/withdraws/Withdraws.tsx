d6378705b50cc50dc4731b23e7053ef890ce8273
mehrab shayetseh <m.shayesteh@yourypto.com>
2021-09-04 14:57:31 +0430

    improvments

--- src/src/app/pages/withdraws/Withdraws.tsx
+++ src/src/app/pages/withdraws/Withdraws.tsx
@@ -1,15 +1,15 @@
 import { lazy } from 'react';
 import { Route } from 'react-router-dom';
+import WithdrawsT from '../../templates/withdraws/WithdrawsT';
 
 const Withdraws = () => {
-  const WithdrawT = lazy(() => import('../../templates/withdraws/WithdrawsT'));
   const WithdrawDetailsT = lazy(
     () => import('../../templates/withdraws/withdraw-details/WithdrawDetailsT')
   );
 
   return (
     <>
-      <Route exact path="/withdraws" component={WithdrawT} />
+      <Route exact path="/withdraws" component={WithdrawsT} />
       <Route exact path="/withdraws/:id" component={WithdrawDetailsT} />
     </>
   );
